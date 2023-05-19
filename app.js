f4d953249fe648babad5e89c3a8ce7acdbe665ff
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-19 15:03:11 -0400

    finished assignment

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
 class Casino {
-    constructor(nate){
-        this.name = nate
+    constructor(name, fakeCoin){
+        this.name = name
         this.timesPlayed = 0
+        this.winnings = 0
+        this.fakeCoin = fakeCoin
     }
     playGame(betAmount){
         this.timesPlayed += 1
-        if(Math.random <= 0.5){
-            console.log('nate wins ' + betAmount)
+        if(Math.random > 0.5 && this.fakeCoin === false) {
+            console.log('you won' + betAmount * (this.timesPlayed) + 'dollars from' + this.name + '!')
+            this.winnings += this.timesPlayed * betAmount
+            console.log('Current winnings: ' + this.winnings)
         }   else{
-            console.log('nate wins the bet amount')   
+            console.log(this.name + "wins!")
+            this.timesPlayed = 0
+            this.winnings = 0   
         }
             
     };
+    rollDice(sides) {
+        console.log(Math.floor(Math.random() * sides) + 1);
+      }
 }
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
+//const myCasino = new Casino("HackerU Casino");
+//console.log(myCasino);
+//myCasino.playGame(5);
+//myCasino.playGame(15);
+//myCasino.playGame(25);
+//myCasino.playGame(35);
+const myCasino = new Casino("HackerU Casino", false);
 myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
-
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(1000);
 // BONUS TESTS
 /*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
